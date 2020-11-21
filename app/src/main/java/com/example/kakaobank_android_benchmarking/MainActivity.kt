24cee590db14d2f7a31a590df0823af1adac440b
fbqweasd <fbqweasd2911@gmail.com>
2020-11-21 21:27:00 +0900

    initial commit

---
+++ app/src/main/java/com/example/kakaobank_android_benchmarking/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.kakaobank_android_benchmarking
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
